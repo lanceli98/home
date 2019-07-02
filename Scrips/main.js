a3c5e5957b2b41dcc65dcfe96b5858f043970b1d
lanceli98 <33961729+lanceli98@users.noreply.github.com>
2019-07-02 16:37:46 -0400

    Update main.js

--- Scrips/main.js
+++ Scrips/main.js
@@ -1,6 +1,13 @@
-new Vue({
+var app = new Vue({
   el: '#app',
   data: {
     message: 'Hello Vue.js!'
+  }
+})
+
+var app2 = new Vue({
+  el: '#app-2',
+  data: {
+    message: 'You loaded this page on ' + new Date().toLocaleString()
   }
 })
